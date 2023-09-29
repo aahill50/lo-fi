50630d622d3c7995fdc8e235427d98e2863c8c0c
Aaron Hill <aahill50@gmail.com>
2023-09-29 02:41:23 -0400

    change tmp file location

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,5 +1,5 @@
 // Our own sqip defaults
-export const OPTION_DEFAULT_NUM_SHAPES = 128;       // numbers of shapes
-export const OPTION_DEFAULT_BLUR = 4;               // gaussian blur level to apply to svg
+export const OPTION_DEFAULT_NUM_SHAPES = 128; // numbers of shapes
+export const OPTION_DEFAULT_BLUR = 4; // gaussian blur level to apply to svg
 
-export const TMP_FOLDER = '/tmp/lo-fi'
+export const TMP_FOLDER = "/tmp"
